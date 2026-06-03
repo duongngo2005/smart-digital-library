48b6ab36216bfea2e576de31c04547c6d5afe603
ngoduong2005 <info.duongngo@gmail.com>
2026-06-03 19:57:47 +0700

    feat(dto): add auth response

---
+++ backend/src/main/java/com/ndd/digitallibrary/dto/response/AuthResponse.java
@@ -0,0 +1,13 @@
+package com.ndd.digitallibrary.dto.response;
+
+import lombok.*;
+
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AuthResponse {
+    private String accessToken;
+    private String refreshToken;
+    private UserResponse userResponse;
+}
