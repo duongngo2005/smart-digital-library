5f6e524446267a7fcc6f5874adfe1f1f985016e4
ngoduong2005 <info.duongngo@gmail.com>
2026-06-04 23:12:55 +0700

    feat(repository): add mothod find by parent is null in category response

--- backend/src/main/java/com/ndd/digitallibrary/repository/CategoryRepository.java
+++ backend/src/main/java/com/ndd/digitallibrary/repository/CategoryRepository.java
@@ -3,5 +3,8 @@ package com.ndd.digitallibrary.repository;
 import com.ndd.digitallibrary.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    List<Category> findByParentIsNull();
 }
