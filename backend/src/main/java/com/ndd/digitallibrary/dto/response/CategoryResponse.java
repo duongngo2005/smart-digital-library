d0213983f1d658f98e068980f819cbe261fbebad
ngoduong2005 <info.duongngo@gmail.com>
2026-06-04 20:16:46 +0700

    feat(dto): add fromEntity mapper and fix parent field in category response

--- backend/src/main/java/com/ndd/digitallibrary/dto/response/CategoryResponse.java
+++ backend/src/main/java/com/ndd/digitallibrary/dto/response/CategoryResponse.java
@@ -3,6 +3,7 @@ package com.ndd.digitallibrary.dto.response;
 import com.ndd.digitallibrary.entity.Category;
 import lombok.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Builder
@@ -14,6 +15,21 @@ public class CategoryResponse {
     private String name;
     private String description;
     private String slug;
-    private Category parent;
+    private Long parentId;
     private List<CategoryResponse> children;
+
+    public static CategoryResponse fromEntity(Category category){
+        return CategoryResponse.builder()
+                .id(category.getId())
+                .name(category.getName())
+                .slug(category.getSlug())
+                .parentId(category.getParent() != null ? category.getParent().getId() : null)
+                .children(
+                        category.getChildren() != null
+                                ? category.getChildren().stream().map(CategoryResponse::fromEntity).toList()
+                                : new ArrayList<>())
+                .description(category.getDescription())
+                .build();
+    }
+
 }
