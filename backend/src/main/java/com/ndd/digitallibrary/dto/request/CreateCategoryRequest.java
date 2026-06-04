45e38326498255a231dfa0791e0ca35d5a6059b8
ngoduong2005 <info.duongngo@gmail.com>
2026-06-04 19:31:53 +0700

    feat(dto): add create category request

---
+++ backend/src/main/java/com/ndd/digitallibrary/dto/request/CreateCategoryRequest.java
@@ -0,0 +1,11 @@
+package com.ndd.digitallibrary.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class CreateCategoryRequest {
+    private String name;
+    private String description;
+    private String parent;
+}
