2ecf6cadc2ce7c96d4cef4c0f5d8a74645405673
ngoduong2005 <info.duongngo@gmail.com>
2026-06-05 15:28:27 +0700

    feat(dto): add tag dto

---
+++ backend/src/main/java/com/ndd/digitallibrary/dto/response/TagResponse.java
@@ -0,0 +1,21 @@
+package com.ndd.digitallibrary.dto.response;
+
+import com.ndd.digitallibrary.entity.Tag;
+import lombok.*;
+
+@Getter @Setter @Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TagResponse {
+    private Long id;
+    private String name;
+    private String slug;
+
+    public static TagResponse fromEntity(Tag tag){
+        return TagResponse.builder()
+                .id(tag.getId())
+                .name(builder().name)
+                .slug(tag.getSlug())
+                .build();
+    }
+}
