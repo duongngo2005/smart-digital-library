eb9e7be4cf2bfc86c9a3eedd9880c6c9b2b0fdb1
ngoduong2005 <info.duongngo@gmail.com>
2026-06-08 16:13:40 +0700

    feat(service): add user service

---
+++ backend/src/main/java/com/ndd/digitallibrary/service/UserService.java
@@ -0,0 +1,99 @@
+package com.ndd.digitallibrary.service;
+
+import com.ndd.digitallibrary.dto.request.ChangePasswordRequest;
+import com.ndd.digitallibrary.dto.request.UpdateProfileRequest;
+import com.ndd.digitallibrary.dto.response.CloudinaryResponse;
+import com.ndd.digitallibrary.dto.response.UserResponse;
+import com.ndd.digitallibrary.entity.User;
+import com.ndd.digitallibrary.enums.SubscriptionTier;
+import com.ndd.digitallibrary.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final CloudinaryService cloudinaryService;
+    private final PasswordEncoder encoder;
+
+    @Transactional
+    public void checkAndSyncUserState(User user){
+        if(user.getSubscriptionTier() != SubscriptionTier.MEMBER
+                && user.getSubscriptionUntil() != null
+                && user.getSubscriptionUntil().isBefore(LocalDateTime.now())){
+
+            user.setSubscriptionTier(SubscriptionTier.MEMBER);
+            user.setSubscriptionStartAt(null);
+            user.setSubscriptionUntil(null);
+            user.setCurrentCycleEnd(null);
+            user.setDownloadedThisMonth(0);
+            userRepository.save(user);
+        }
+        if(user.getCurrentCycleEnd() != null && LocalDateTime.now().isAfter(user.getCurrentCycleEnd())){
+            user.setDownloadedThisMonth(0);
+            user.setCurrentCycleEnd(user.getCurrentCycleEnd().plusMonths(1));
+            userRepository.save(user);
+        }
+    }
+
+    public UserResponse getMyUserProfile(Long userId){
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("Không tìm thấy người dùng này"));
+
+        return UserResponse.fromEntity(user);
+    }
+
+    @Transactional
+    public UserResponse updateProfile(Long userId, UpdateProfileRequest request){
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("Không tìm thấy người dùng này"));
+
+        user.setFullName(request.getFullName());
+        return UserResponse.fromEntity(userRepository.save(user));
+    }
+
+    @Transactional
+    public UserResponse updateAvatar(Long userId, MultipartFile avatar){
+
+        if(avatar == null || avatar.isEmpty()){
+            throw new RuntimeException("File ảnh không đươc để trống");
+        }
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("Không tìm thấy người dùng này"));
+
+        if(user.getAvatarUrl() != null && user.getAvatarPublicId() != null){
+            cloudinaryService.deleteFile(user.getAvatarPublicId(), "image");
+        }
+
+        CloudinaryResponse response = cloudinaryService.uploadAvatar(avatar);
+        user.setAvatarUrl(response.getUrl());
+        user.setAvatarPublicId(response.getPublicId());
+
+        return UserResponse.fromEntity(userRepository.save(user));
+    }
+
+    @Transactional
+    public void changePassword(Long userId, ChangePasswordRequest request){
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("Không tìm thấy người dùng này"));
+
+        if (!encoder.matches(request.getCurrentPassword(), user.getPassword())){
+            throw new RuntimeException("Mật khẩu hiện tại không đúng");
+        }
+        if(!request.getNewPassword().equals(request.getConfirmPassword())){
+            throw new RuntimeException("Mật khẩu mới và xác nhận mật khẩu không trùng khớp");
+        }
+
+        user.setPassword(encoder.encode(request.getNewPassword()));
+
+        userRepository.save(user);
+    }
+}
