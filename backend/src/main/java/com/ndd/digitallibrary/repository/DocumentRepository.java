05bd5f429a954ff4717435eb11f2cfad2b084ab8
ngoduong2005 <info.duongngo@gmail.com>
2026-06-10 17:36:02 +0700

    feat(repository): extends JpaSpecifiaction in DocumentRepository

--- backend/src/main/java/com/ndd/digitallibrary/repository/DocumentRepository.java
+++ backend/src/main/java/com/ndd/digitallibrary/repository/DocumentRepository.java
@@ -2,8 +2,9 @@ package com.ndd.digitallibrary.repository;
 
 import com.ndd.digitallibrary.entity.Document;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface DocumentRepository extends JpaRepository<Document, Long> {
+public interface DocumentRepository extends JpaRepository<Document, Long>, JpaSpecificationExecutor<Document> {
 
     boolean existsByTitleAndAuthorAndPublisherAndPublishedYear(String title, String author, String publisher, Integer publishYear);
 
